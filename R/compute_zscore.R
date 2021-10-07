c747489eb81711ad7ac5b3a0532cc6896758bc8d
Acare <andrea.carenzo@gmail.com>
2021-10-07 16:34:08 +0200

    Modified compute_zscore() and tested against GSVA::gsva(..., method = zscore). Modified DESCRIPTION with authors, package title and description.

--- R/compute_zscore.R
+++ R/compute_zscore.R
@@ -4,36 +4,50 @@
 #'
 #' @param expr_data A gene expression matrix (or data frame) with gene symbols as
 #'  row names and samples as columns.
-#' @param signature A list of character vectors corresponding to gene signatures.
+#' @param signature A list of named character vectors corresponding to gene signatures.
 #'
-#' @return A data frame with
+#' @return A tibble with the first column indicating sample identifiers (`sample_id`)
+#'  and all the other columns ....
 #' @export
 #'
 #' @examples
-#' m <- matrix()
+#' set.seed(123)
+#'
+#' test_expr <- data.frame(
+#'     sample1 = rnorm(10, mean = 8, sd = 2),
+#'     sample2 = rnorm(10, mean = 7, sd = 2),
+#'     sample3 = rnorm(10, mean = 8, sd = 3),
+#'     row.names = paste0("gene", 1:10)
+#' )
+#'
+#' test_list <- list(
+#'     gs1 = paste0("gene", c(1, 2, 6)),
+#'     gs2 = paste0("gene", 1:5),
+#'     gs3 = paste0("gene", c(3, 1, 9)),
+#'     gs4 = paste0("gene", c(1, 3, 9))
+#' )
+#'
+#' compute_zscore(test_expr, test_list)
+
 compute_zscore <- function(expr_data, signatures) {
 
     single_sig_zscore <- function(dataset, genes) {
         filt_data <- dataset[genes, ]
-        scaled_data <- scale(t(filt_data), center = TRUE, scale = TRUE)
-        zscore_vec <- rowSums(scaled_data) / sqrt(length(genes))
-        zscore_df <-  tibble::enframe(zscore_vec,
-                                      name = "sample_id", value = "zscore")
+        scaled_data <- base::scale(t(filt_data), center = TRUE, scale = TRUE)
+        zscore_vec <- base::rowSums(scaled_data) / sqrt(length(genes))
+        tibble::enframe(zscore_vec, name = "sample_id", value = "zscore")
     }
 
-    result <- lapply(signatures, single_sig_zscore,
-                     dataset = expr_data)
+    result <- base::lapply(
+        signatures,
+        FUN = function(geneset) {
+            single_sig_zscore(dataset = expr_data, genes = geneset)
+        }
+    )
+    result <- dplyr::bind_rows(result, .id = "signature_id")
+    tidyr::pivot_wider(result,
+                       id_cols = sample_id,
+                       names_from = signature_id,
+                       values_from = zscore)
 
-    result <- bind_rows(result, .id = "signature_id")
-    result <- tidyr::pivot_wider(result, sample_id,
-                                 names_from = signature_id,
-                                 values_from = zscore)
-    result
-
-    # filt_data <- expr_data[signatures, ]
-    # scaled_data <- scale(t(filt_data), center = TRUE, scale = TRUE)
-    # zscore_vec <- rowSums(scaled_data) / sqrt(length(signatures))
-    # result <-  tibble::enframe(zscore_vec, name = "sample_id", value = "zscore")
-    #
-    # result
 }
